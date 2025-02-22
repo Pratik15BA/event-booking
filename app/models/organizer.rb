7efe652ef567518d7f09f525ef92dd53ca93ec92
Shailesh Parkhi <shailesh.parkhi@joshsoftware.com>
2025-02-22 17:25:49 +0530

    added validations on organizer and user

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -1,3 +1,6 @@
 class Organizer < ApplicationRecord
     has_many :events
+    validates :name, presence: true
+    validates :email_id, presence: true
+    validates :contact_no, presence: true
 end
